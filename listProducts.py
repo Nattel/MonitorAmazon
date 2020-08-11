79e3160c2cf3f010776d463681716481dd245c3f
José Renato Lara <j.renato.lara@ibm.com>
2020-08-10 22:16:25 -0300

    upload to mongo and scrapper as class

--- listProducts.py
+++ listProducts.py
@@ -1,52 +1,66 @@
 import requests
+from datetime import datetime
 from bs4 import BeautifulSoup
 import re
 
-def listConnection(url):
-	s = requests.session()
-	r = s.get(url)
+class GetSite():
+	def __init__(self, url):
+		s = requests.session()
+		r = s.get(url)
 
-	return BeautifulSoup(r.text, 'html.parser')
+		self.soup = BeautifulSoup(r.text, 'html.parser')
 
-def listBooks(soup):
-	# aims first for the 'center' div where the books are and then enlists them
-	divs = soup.find('div', {'id': 'content-right'}).find('div', {'id': 'item-page-wrapper'})
-	lis = divs.find('ul').findAll('li')
+	def listBooks(self):
+		# aims first for the 'center' div where the books are and then enlists them
+		divs = self.soup.find('div', {'id': 'content-right'}).find('div', {'id': 'item-page-wrapper'})
+		self.lis = divs.find('ul').findAll('li')
 
-	return lis
+		return self.lis
 
+	def getsBookInfo(self, li):
+		authorInfo = li.find('span', {'class': 'a-size-base'}).getText()
+		self.infos = {
+			'name': li.find('h3', {'class': 'a-size-base'}).getText(),
+			'author': re.split(r'[,(]', authorInfo[3:len(authorInfo)])[0]
+		}
 
-def getsBookInfo(li):
-	authorInfo = li.find('span', {'class': 'a-size-base'}).getText()
-	infos = {
-		'name': li.find('h3', {'class': 'a-size-base'}).getText(),
-		'author': re.split(r'[,(]', authorInfo[3:len(authorInfo)])[0]
-	}
+		price = li.find('span', {'class': 'a-offscreen'})
+		priority = li.find(string='Prioridade:').find_parent().findAll('span')[1]
 
-	price = li.find('span', {'class': 'a-offscreen'})
-	priority = li.find(string='Prioridade:').find_parent().findAll('span')[1]
+		if price: self.infos.update({'price': price.getText()})
+		if priority: self.infos.update({'priority': priority.getText()})
 
-	if price: infos.update({'price': price.getText()})
-	if priority: infos.update({'priority': priority.getText()})
+		self.infos.update(self.deliveryInfos(li))
 
-	infos.update(deliveryInfos(li))
+		return self.infos
 
-	return infos
-
-def deliveryInfos(li):
-	price = '$0,00'
-	delivery = li.find('span', {'class':'wl-item-delivery-badge'})
-	if (delivery):
-		freeDelivery = delivery.find('a')
-		if(freeDelivery):
-			prime = True
+	def deliveryInfos(self, li):
+		price = '$0,00'
+		delivery = li.find('span', {'class':'wl-item-delivery-badge'})
+		if (delivery):
+			freeDelivery = delivery.find('a')
+			if(freeDelivery):
+				prime = True
+			else:
+				price = re.split(r'[+ \n]', delivery.getText())[2]
+				prime = False
+				return {'prime': prime, 'delivery': price}
 		else:
-			price = re.split(r'[+ \n]', delivery.getText())[2]
 			prime = False
-			return {'prime': prime, 'delivery': price}
-	else:
-		prime = False
 
-	return {'prime': prime}
+		return {'prime': prime}
 
+	def	getAllInfos(self):
+		allInfo = {
+			'moment': datetime.now(),
+			'books': []
+		}
+		try:
+			ul = self.listBooks()
+			for li in ul:
+				allInfo['books'].append(self.getsBookInfo(li))
+		except:
+			print(TypeError('Undefined Error Occuring: sometimes listBooks() gets soup variable as NoneType. God knows why!'))
+
+		return allInfo
 # r2 = s.get('https://images-na.ssl-images-amazon.com/images/I/61-6nKPKyWL.js?AUIClients/AmazonUIjQuery')
--- main.py
+++ main.py
@@ -1,10 +1,28 @@
+from pymongo import MongoClient
 from listProducts import *
 from dotenv import load_dotenv
+from pprint import pprint
 import os
 
 load_dotenv(dotenv_path='.env')
-url = os.getenv('url')
-con = listConnection(url)
 
-for li in listBooks(con):
-	print(getsBookInfo(li))
+# getting site information
+url = os.getenv('url')
+site = GetSite(url)
+
+# connecting with MongoDB
+user = os.getenv('user')
+password = os.getenv('password')
+cluster = os.getenv('cluster')
+
+conString = 'mongodb+srv://'+user+':'+password+'@'+cluster+'.kivsk.gcp.mongodb.net'
+client = MongoClient(conString)
+db = client.get_database('amazon')
+
+if url not in db.list_collection_names():
+	db.create_collection(url)
+col = db.get_collection(url)
+
+
+allInfo = site.getAllInfos()
+if allInfo: col.insert_one(allInfo)
